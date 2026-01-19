ff2c3b38f214830f239c5caa4ac7d48aa55516dc
Adrien Narkiewicz <adrien.narkiewicz@celadodc-rswl.com>
2026-01-19 18:36:43 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { invoke } from "@tauri-apps/api/core";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// These contents can be copy-pasted below the existing code, don't replace the entire file!!
+
+// Utility function to implement a sleep function in TypeScript
+function sleep(seconds: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+// Setup function
+async function setup() {
+  // Fake perform some really heavy setup task
+  console.log("Performing really heavy frontend setup task...");
+  await sleep(3);
+  console.log("Frontend setup task complete!");
+  // Set the frontend task as being completed
+  invoke("set_complete", { task: "frontend" });
+}
+
+// Effectively a JavaScript main function
+window.addEventListener("DOMContentLoaded", () => {
+  setup();
+});
